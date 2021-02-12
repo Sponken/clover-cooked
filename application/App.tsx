5a69a7fc87971ada96767ef1f6bfcb0b20d0783f
erathiian <42635802+erathiian@users.noreply.github.com>
2021-02-12 08:39:13 +0100

    Formaterade lite filer

--- application/App.tsx
+++ application/App.tsx
@@ -2,15 +2,15 @@
  * App.tsx motsvarar "main"
  * Försök att inte skriva kod i App som hade kunnat skrivas i src
  */
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import { Home } from './src/screens';
+import { Home } from "./src/screens";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <Home/>
+      <Home />
     </SafeAreaProvider>
   );
 }
--- application/src/screens/Home.tsx
+++ application/src/screens/Home.tsx
@@ -2,27 +2,24 @@
  * Hem skärm, första skärmen som visas vid vanlig öppning av appen
  */
 
-import { StyleSheet, Text, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
 
 export function Home() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello World!</Text>
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
-
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
-  
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});

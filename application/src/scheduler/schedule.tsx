b0e82daf005562e6bfa0a57ef4fc0dad3fd1edd4
Pazzaz <pazzaz.sundqvist@gmail.com>
2021-03-04 16:45:31 +0100

    Förbättra den komplicerade scheduling-metoden

--- application/src/scheduler/schedule.tsx
+++ application/src/scheduler/schedule.tsx
@@ -1,62 +1,65 @@
-import { type } from "os"
-import * as recipe from "../../data/recipes/gräddtårta.json"
-//import {Task} from "../data"
-
-type Task = string
+import { Recipe, Task } from "../data";
 
 
-let noOfCooks: number = 2
+type TaskId = string
 
-let schedule = (): void => {
+// TANKE: Pontus frågor
+// - Jag förstår inte riktigt koden överlag än men typer är mer logiska nu.
+// - Vi jobbar hela tiden med id på tasks och har sedan flera `Map`s som har information om tasks. Men borde vi inte bara skicka runt tasks istället?
+// - Hur ska vi hantera att vissa tasks håller på just nu? Ska de ligga i `W`? Man skulle kunna tänka sig att algoritmen redan "gått några iterationer" och fördelat ut uppgifter.
+//   Men då måste vi också veta hur länge personer har hållt på eller? För att kunna säga hur länge mer de kommer jobba.
+// - Completed tasks borde läggas i `C` iallafall
+// - Vart är A (De tasks som är "aktiva" vid en specifik tidpunkt)? A används i `nextTime` men det som skickas in är `W[g - 1].flat()`. Hur skiljer sig algoritmen från det som står i pappret?
+function calculate(
+  recipe: Recipe,
+  completedTasks: Task[],
+  noOfCooks: number,
+): Task {
+  let j: TaskId[] = allTasks(recipe)        // All tasks(jobs) // TODO Change to un-started tasks to make eligible tasks more efficient?
+  let g: number = 0                       // Iteration
+  let W: TaskId[][] = [[]]                    // Tasks assigned to a given iteration and worker(in that order). Only one task per worker and iteration. "" represents no task being done
+  let P: Map<TaskId, TaskId[]>                         // Presedence/dependcy over tasks. 
+  let SP: Map<string, string[]>                         // Strong presedence/dependency over tasks.
+  [P, SP] = dependencies(recipe);
+  let C: TaskId[][] = [["start"]]             // Tasks that have been completed at a given iteration
+  let t: number[] = [0]                     // Timestamp for a given iteration
+  let F: Map<TaskId, number> = new Map()               // Finish times for tasks
+  let D: TaskId[][] = [[]]                    // The eligble tasks at given iteration
+  let p: Map<TaskId, number> = timeEstimations(recipe) // The estimated time of completion for a given task
 
-  calculate()
-
-}
-
-let calculate = () => {
-
-  let j: Task[] = allTasks() // All tasks(jobs) // TODO Change to un-started tasks to make eligible tasks more efficient?
-  let g: number = 0 // Iteration
-  let W: Task[][] = [[]] //Tasks assigned to a given iteration and worker(in that order). Only one task per worker and iteration. "" represents no task being done
-  let P: Map<Task, Task[]>// Presedence/dependcy over tasks. 
-  let SP: Map<string, string[]> // Strong presedence/dependcy over tasks. 
-  [P, SP] = dependencies()
-  let C: Task[][] = [["start"]]  // Tasks that have been completed at a given iteration
-  let t: number[] = [0]   // Timestamp for a given iteration
-  let F: Map<Task, number> = new Map() // Finish times for tasks
-  let D: Task[][] = [[]] // The eligble tasks at given iteration
-  let p: Map<Task, number> = timeEstimations() // The estimated time of completion for a given task
-
+  // TANKE: Varför just det här kriteriet? Det är väl inte vad som står i pappret?
   while (C.flat().length < j.length) {
-    g++
+    g++;
 
-    t[g] = nextTime(W[g - 1].flat(), F)
-    C.push([])
-    W[g] = new Array(noOfCooks).fill("")
+    t[g] = nextTime(W[g - 1].flat(), F);
+    C.push([]);
+    W[g] = new Array(noOfCooks).fill("");
 
-    completeTasks(W, C[g], g, t, F)
-    D[g] = eligibleTasks(C, W, j, g, P, SP)
+    completeTasks(W, C[g], g, t, F, noOfCooks);
+    D[g] = eligibleTasks(C, W, j, g, P, SP);
 
-    while (D[g].length > 0 && freeWorkers(W[g])) {
-      let newTask: Task = D[g].pop() ?? ""
-      F.set(newTask, p.get(newTask) + t[g])
-      addTask(W[g], newTask)
-      D[g] = eligibleTasks(C, W, j, g, P, SP)
+    while (D[g].length > 0 && freeWorkers(W[g], noOfCooks)) {
+      let newTask: TaskId = D[g].pop() ?? "";
+      F.set(newTask, p.get(newTask) + t[g]);
+      addTask(W[g], newTask, noOfCooks);
+      D[g] = eligibleTasks(C, W, j, g, P, SP);
     }
   }
 
-  printSchedule(W, t)
+
+  printSchedule(W, t);
+  return W[0][0] // TANKE: Hur väljer man den?
   console.log("Tasks that are not completed: ")
   console.log(nonCompletedTasks(j, C.flat()))
 }
 
-const nonCompletedTasks = (tasks: Task[], completed: Task[]): Task[] => {
+const nonCompletedTasks = (tasks: TaskId[], completed: TaskId[]): TaskId[] => {
   return tasks.filter(task => !completed.includes(task))
 
 }
 
 
-const freeWorkers = (W: Task[]): boolean => {
+const freeWorkers = (W: TaskId[], noOfCooks: number): boolean => {
   for (let i = 0; i < noOfCooks; i++) {
     if (W[i] === "")
       return true
@@ -64,7 +67,7 @@ const freeWorkers = (W: Task[]): boolean => {
   return false
 }
 
-const addTask = (W: Task[], t: Task) => {
+const addTask = (W: TaskId[], t: TaskId, noOfCooks: number) => {
   for (let i = 0; i < noOfCooks; i++) {
     if (W[i] === "") {
       W[i] = t
@@ -75,37 +78,23 @@ const addTask = (W: Task[], t: Task) => {
 
 // Find the tasks eligible for assignment
 // FUNKAR INTE KORREKT
-const eligibleTasks = (C: string[][], W: Task[][], tasks: Task[], g: number, P: Map<Task, Task[]>, SP: Map<Task, Task[]>): string[] => {
-  let eligibleTasks: Task[] = []
-  let strongEligibleTasks: Task[] = []
+function eligibleTasks(C: string[][], W: TaskId[][], tasks: TaskId[], g: number, P: Map<TaskId, TaskId[]>, SP: Map<TaskId, TaskId[]>): string[] {
+  let eligibleTasks: TaskId[] = []
+  let strongEligibleTasks: TaskId[] = []
   let anyStrong = false
 
 
   for (let i = 0; i < tasks.length; i++) {
     let task = tasks[i]
-    /*if (C.flat().includes(task)) // Check if it's complete
-      continue
-    else if (W[g].flat().includes(task)) // Check if it's currently active
-      continue
-    else if (!includesAll(C.flat(), P.get(task))) // Checks if any dependencies aren't complete
-      continue
-    else if (includesAny(C.flat(), SP.get(task) ?? [])) { // If there are any strong dependencies, add them to array
-      strongEligibleTasks.push(task)
-      anyStrong = true
-      continue
-    }*/
-
-    if (isEligible(task, C.flat(), W.flat(), P, SP))
+    if (isEligible(task, C.flat(), W.flat(), P, SP)) {
       eligibleTasks.push(task)
-
+    }
   }
-
-
 
   return anyStrong ? strongEligibleTasks : eligibleTasks // If any task has all it's strong depencies completed, only strong tasks are eligible
 }
 
-const isEligible = (task: Task, completedTasks: Task[], currentTasks: Task[], weakDependencies: Map<Task, Task[]>, strongDependencies: Map<Task, Task[]>): boolean => {
+const isEligible = (task: TaskId, completedTasks: TaskId[], currentTasks: TaskId[], weakDependencies: Map<TaskId, TaskId[]>, strongDependencies: Map<TaskId, TaskId[]>): boolean => {
   if (completedTasks.includes(task)) {
     return false
   }
@@ -119,17 +108,16 @@ const isEligible = (task: Task, completedTasks: Task[], currentTasks: Task[], we
   return true
 }
 
-const includesAny = (superArr: any[], subArr: any[]): boolean => subArr.some(e => 
+const includesAny = (superArr: any[], subArr: any[]): boolean => subArr.some(e =>
   superArr.includes(e)
 )
 
-const includesAll = (superArr: any[], subArr: any[]): boolean => subArr.every(e => 
+const includesAll = (superArr: any[], subArr: any[]): boolean => subArr.every(e =>
   superArr.includes(e)
 )
-
 
 // Updates the completed tasks and the current tasks
-const completeTasks = (W: Task[][], C: Task[], g: number, t: number[], F: Map<Task, number>) => {
+const completeTasks = (W: TaskId[][], C: TaskId[], g: number, t: number[], F: Map<TaskId, number>, noOfCooks: number) => {
 
   for (let i = 0; i < noOfCooks; i++) {
     let oldTask = W[g - 1][i]
@@ -140,28 +128,10 @@ const completeTasks = (W: Task[][], C: Task[], g: number, t: number[], F: Map<Ta
       W[g][i] = oldTask
     }
   }
-
-  /*
-    W[g].forEach(worker =>  worker.forEach(task =>{
-      if(t[g] >= F.get(task)){
-        C[g].push(task)
-        worker.re
-      }
-    }))
-  
-    W.forEach(worker => {
-      worker[g-1].forEach(task => {
-        if(t[g] >= F.get(task)) {
-          C[g].push("task") 
-        } 
-        else worker[g].push("task")
-      })
-    });
-  */
 }
 
 // Hitta tiden för nästa iteration. Denna tid är då nästa task är klar
-const nextTime = (A: string[], F: Map<Task, number>): number => {
+const nextTime = (A: string[], F: Map<TaskId, number>): number => {
   if (!A?.length)
     return 0
 
@@ -172,31 +142,22 @@ const nextTime = (A: string[], F: Map<Task, number>): number => {
 }
 
 
-let timeEstimations = () => {
-  let estimations: Map<Task, number> = new Map()
+let timeEstimations = (recipe: Recipe) => {
+  let estimations: Map<TaskId, number> = new Map()
 
   recipe.tasks.forEach(task => estimations.set(task.id, task.estimatedTime))
   return estimations
 }
 
-let resources = () => {
-  //Where do we get availability data?
-  let resources = recipe.resources.map(r => { return { "id": r.id, "name": r.name, "availability": 1 } })
-  resources.push({ "id": "workers", "name": "Cooks", "availability": noOfCooks })
-  /*for (let i = 0; i < noOfCooks; i++) {
-    resources.push({"id": "cookNo" + i,"name": "Cook " + i})
-  }
-  */
-  return resources
-}
+
 
 // All dependencies, including the ones to the implicit "start" and "end" nodes
 // Returns in the form of an array with two values, [dependencies, strongDependecies]
-let dependencies = (): Map<string, string[]>[] => {
+let dependencies = (recipe: Recipe): [Map<TaskId, TaskId[]>, Map<string, string[]>] => {
 
   let depMap: Map<string, string[]> = new Map()
   let strongDepMap: Map<string, string[]> = new Map()
-  depMap.set("end", finalTasks().map(task => task.id))
+  depMap.set("end", finalTasks(recipe).map(task => task.id))
 
   recipe.tasks.forEach(task => {
     depMap.set(task.id, [])
@@ -209,8 +170,8 @@ let dependencies = (): Map<string, string[]>[] => {
   })
 
   // Add "start" as a dependency to all starting tasks
-  startTasks().forEach(startT => {
-    if (startT.initalTask) // If they are inital tasks, they are to be done right after start
+  startTasks(recipe).forEach(startT => {
+    if (startT.initialTask) // If they are inital tasks, they are to be done right after start
       strongDepMap.get(startT.id)?.push("start")
     else
       depMap.get(startT.id)?.push("start")
@@ -220,25 +181,25 @@ let dependencies = (): Map<string, string[]>[] => {
   return [depMap, strongDepMap]
 }
 
-let allTasks = (): string[] => recipe.tasks.map(task => task.id)
+let allTasks = (recipe: Recipe): string[] => recipe.tasks.map(task => task.id);
 
 
 
 
 // All tasks that can be done at the start, eg. all tasks with no dependencies
-let startTasks = () => {
+let startTasks = (recipe: Recipe) => {
   let startTasks = recipe.tasks.filter(task => !recipe.taskDependencies.some(dep => (dep.taskId == task.id || dep.taskId == task.id)))
   return startTasks
 }
 
-let finalTasks = () => {
+let finalTasks = (recipe: Recipe) => {
 
   return recipe.tasks.filter((task) => {
     return task.finalTask ?? false
   })
 }
 
-const printSchedule = (W: Task[][], t: number[]) => {
+const printSchedule = (W: TaskId[][], t: number[]) => {
   console.log("Detta ska göras")
   for (let g = 0; g < W.length; g++) {
     console.log(" Tid " + t[g] + ":")
@@ -249,9 +210,13 @@ const printSchedule = (W: Task[][], t: number[]) => {
   }
 }
 
-
-export const setNoOfCooks = (no: number) => noOfCooks = no
-
-export const getNoOfCooks = (): number => noOfCooks
-
-export default schedule
+let resources = (recipe: Recipe, noOfCooks: number) => {
+  //Where do we get availability data?
+  let resources = recipe.resources.map(r => { return { "id": r.id, "name": r.name, "availability": 1 } })
+  resources.push({ "id": "workers", "name": "Cooks", "availability": noOfCooks })
+  /*for (let i = 0; i < noOfCooks; i++) {
+    resources.push({"id": "cookNo" + i,"name": "Cook " + i})
+  }
+  */
+  return resources
+}
